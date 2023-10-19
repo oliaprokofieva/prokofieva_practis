a4d4dfaa1d7d00c9d1543179b3b755edc6db016c
vikastacenko <146466063+vikastacenko@users.noreply.github.com>
2023-10-19 22:45:05 +0300

    Delete pr4,5 directory

--- pr4,5/backend/routes/ToDoRoute.js
+++
@@ -1,6 +0,0 @@
-const {Router} = require("express");
-const { getToDo } = require("../controllers/ToDoController");
-const router = Router()
-router.get('/', getToDo)
-router.post('/save', saveToDo)
-module.exports = router;
